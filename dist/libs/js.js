c2e52e5f49a2a0e19c20331f480bd98641b358f1
VolodymyrTrykoz <mr.mastachok@gmail.com>
2017-12-06 23:11:30 +0200

    color changing and style playing

--- dist/libs/js.js
+++ dist/libs/js.js
@@ -1,5 +1,8 @@
-(function none(){
+
+function none(){
     var x = document.querySelector('.container__description--shift');
     x.style.transform = 'translateY(0)';
     x.style.opacity = '1';
-})()
+}
+
+
